82463e84306e2ab041f8091e8411c920323c1128
Kamal Kishor Kumar <kamalkishorkumar@Kamals-MacBook-Pro.local>
2023-01-12 13:02:40 +0530

    All basic functions done

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,6 @@
 import 'package:bloc_counter/logic/cubit/counter_cubit.dart';
+import 'package:bloc_counter/presentation/screen/secondscreen.dart';
+import 'package:bloc_counter/presentation/screen/thirdscreen.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 
---
+++ lib/presentation/screen/thirdscreen.dart
@@ -0,0 +1,77 @@
+import 'package:bloc_counter/presentation/screen/secondscreen.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+
+import '../../logic/cubit/counter_cubit.dart';
+
+// import '../cubit/counter_cubit.dart';
+
+class ThirdScreen extends StatefulWidget {
+  const ThirdScreen({super.key});
+
+  @override
+  State<ThirdScreen> createState() => _ThirdScreenState();
+}
+
+class _ThirdScreenState extends State<ThirdScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                ElevatedButton(
+                  onPressed: () {
+                    BlocProvider.of<CounterCubit>(context).decrement();
+                  },
+                  child: const Text("-"),
+                ),
+                const SizedBox(width: 15),
+                BlocConsumer<CounterCubit, CounterState>(
+                  listener: (context, state) {
+                    if (state.wasIncremented!) {
+                      ScaffoldMessenger.of(context).showSnackBar(const SnackBar(
+                        content: Text("Incremented"),
+                      ));
+                    } else {
+                      ScaffoldMessenger.of(context).showSnackBar(const SnackBar(
+                        content: Text("Decremented"),
+                      ));
+                    }
+                  },
+                  builder: (context, state) {
+                    return Text(
+                      state.counterValue.toString(),
+                      style: const TextStyle(
+                          fontSize: 40, fontWeight: FontWeight.bold),
+                    );
+                  },
+                ),
+                const SizedBox(width: 15),
+                ElevatedButton(
+                    onPressed: () {
+                      BlocProvider.of<CounterCubit>(context).increment();
+                    },
+                    child: const Text("+"))
+              ],
+            ),
+            const SizedBox(height: 20),
+            ElevatedButton(
+                onPressed: () {
+                  Navigator.of(context).push(MaterialPageRoute(
+                      builder: (_) => BlocProvider.value(
+                            value: BlocProvider.of<CounterCubit>(context),
+                            child: const SeconScreen(),
+                          )));
+                },
+                child: const Text("Go to Second screen"))
+          ],
+        ),
+      ),
+    );
+  }
+}
